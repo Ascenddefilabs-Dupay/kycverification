8eca7b8b00160e9085e12e119004146ead6acc30
Ganesh-Mirashi <ganesh.mirashi@ascenddefi.com>
2024-08-28 11:26:41 +0530

    I have changed the api in backend

--- backend-kycverification/backend-kycverification/urls.py
+++ backend-kycverification/backend-kycverification/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('kycverification.urls')),
+    path('kycverification_api/',include('kycverification.urls')),
 ]
