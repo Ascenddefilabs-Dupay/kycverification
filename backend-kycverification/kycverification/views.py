7b9b572600cb275c33b66965135f977c70446fa0
Ganesh-Mirashi <ganesh.mirashi@ascenddefi.com>
2024-09-03 09:55:11 +0530

    added details based on user id in kyc verification

--- backend-kycverification/kycverification/views.py
+++ backend-kycverification/kycverification/views.py
@@ -65,20 +65,77 @@ class KYCDetailViewSet(viewsets.ModelViewSet):
     def get_user_id(self, user_id):
         print(user_id)
 
+# class UsersViewSet(viewsets.ModelViewSet):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = CustomUserSerializer
+
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.save()
+
+#         # Generate KYC details only with user_id and kyc_id
+#         kyc_data = {
+#             'user_id': user.user_id,
+#             'kyc_id': self.generate_kyc_id(user.user_id),
+#             'country': None,
+#             'document_type': None,
+#             'adhar_number': None,
+#             'adhar_front_image': None,
+#             'adhar_back_image': None,
+#             'pan_number': None,
+#             'pan_front_image': None,
+#             'pan_back_image': None,
+#             'investment': None,
+#             'nfts': None,
+#             'web3': None,
+#         }
+#         kyc_view_set = KYCDetailViewSet()
+#         kyc_view_set.get_user_id(user.user_id)
+        
+#         kyc_serializer = KYCDetailSerializer(data=kyc_data)
+#         kyc_serializer.is_valid(raise_exception=True)
+#         kyc_serializer.save()
+
+#         headers = self.get_success_headers(serializer.data)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+#     def generate_kyc_id(self, user_id):
+#         latest_kyc = KYCDetail.objects.order_by('-kyc_id').first()
+#         if latest_kyc and re.search(r'\d+', latest_kyc.kyc_id):
+#             last_id = latest_kyc.kyc_id
+#             number = int(re.search(r'\d+', last_id).group())
+#             new_number = number + 1
+#             return f'kycC{new_number:04d}'
+#         return f'kycC0001'
+    
+
+
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        # Check if a user with the given user_id already exists
+        user_instance = CustomUser.objects.filter(user_id=request.data.get('user_id')).first()
+
+        if user_instance:
+            # If user exists, update the user details
+            serializer = self.get_serializer(user_instance, data=request.data, partial=True)
+        else:
+            # If user does not exist, create a new user
+            serializer = self.get_serializer(data=request.data)
+        
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
 
-        # Generate KYC details only with user_id and kyc_id
+        # Check if KYC details already exist for the given user_id
+        kyc_instance = KYCDetail.objects.filter(user_id=user.user_id).first()
+        
         kyc_data = {
             'user_id': user.user_id,
-            'kyc_id': self.generate_kyc_id(user.user_id),
-            'country': None,
+            'kyc_id': self.generate_kyc_id(user.user_id) if not kyc_instance else kyc_instance.kyc_id,
+            'country': request.data.get('country'),
             'document_type': None,
             'adhar_number': None,
             'adhar_front_image': None,
@@ -90,15 +147,17 @@ class UsersViewSet(viewsets.ModelViewSet):
             'nfts': None,
             'web3': None,
         }
-        kyc_view_set = KYCDetailViewSet()
-        kyc_view_set.get_user_id(user.user_id)
+
+        if kyc_instance:
+            kyc_serializer = KYCDetailSerializer(kyc_instance, data=kyc_data, partial=True)
+        else:
+            kyc_serializer = KYCDetailSerializer(data=kyc_data)
         
-        kyc_serializer = KYCDetailSerializer(data=kyc_data)
         kyc_serializer.is_valid(raise_exception=True)
         kyc_serializer.save()
 
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status=status.HTTP_200_OK if user_instance else status.HTTP_201_CREATED, headers=headers)
 
     def generate_kyc_id(self, user_id):
         latest_kyc = KYCDetail.objects.order_by('-kyc_id').first()
@@ -108,4 +167,3 @@ class UsersViewSet(viewsets.ModelViewSet):
             new_number = number + 1
             return f'kycC{new_number:04d}'
         return f'kycC0001'
-    
